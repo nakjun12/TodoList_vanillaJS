50debeb129370e181dd6b80127dfe5b529d30d95
nakjun <111031253+nakjun12@users.noreply.github.com>
2023-06-15 17:24:01 +0900

    Feat : select 이벤트 추가

--- React_To_Do_List/src/component/spatialNavigation/MenuItems.tsx
+++ React_To_Do_List/src/component/spatialNavigation/MenuItems.tsx
@@ -1,21 +1,51 @@
 import { useFocusable } from "@noriginmedia/norigin-spatial-navigation";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import styles from "./spatialComponent.module.css";
-type Props = { item: string };
+type Props = {
+  item: string;
+  choice: string;
+  setChoice: Dispatch<SetStateAction<string>>;
+};
 
-export default function MenuItems({ item }: Props) {
+export default function MenuItems({ item, choice, setChoice }: Props) {
+  const [className, setClassName] = useState(styles.Vertical_State_Default);
+
   const { ref, focused, setFocus } = useFocusable({
     focusable: true,
     focusKey: item,
-  });
-  console.log(ref, focused, item);
-  return (
-    <span
-      ref={ref}
-      className={
-        focused ? styles.Vertical_State_Focus : styles.Vertical_State_Default
+    onEnterPress: () => {
+      console.log("히히히"); //아래이동 추가하기
+      setChoice(item);
+    },
+    onArrowPress: (direction) => {
+      // direction 값을 확인하여 화살표 방향에 따라 다르게 반응하도록 처리
+      if (direction === "up") {
+        // 위 화살표를 눌렀을 때의 처리
+        console.log("위 화살표가 눌렸을 때 동작");
+        setChoice(""); // 적절한 상태 업데이트를 적용하세요
+      } else if (direction === "down") {
+        // 아래 화살표를 눌렀을 때의 처리
+        console.log("아래 화살표가 눌렸을 때 동작");
+        setChoice(item); // 적절한 상태 업데이트를 적용하세요
       }
-      onClick={() => setFocus(item)}
-    >
+
+      return true;
+    },
+  });
+  console.log(choice, "?", item);
+  useEffect(() => {
+    if (focused && choice === item) {
+      console.log("히히히");
+      setClassName(styles.Vertical_State_Choice);
+    } else if (focused) {
+      setClassName(styles.Vertical_State_Focus);
+    } else {
+      setClassName(styles.Vertical_State_Default);
+    }
+  }, [choice, focused]);
+
+  return (
+    <span ref={ref} className={className} onClick={() => setFocus(item)}>
       {item}
     </span>
   );
--- React_To_Do_List/src/component/spatialNavigation/Tags.tsx
+++ React_To_Do_List/src/component/spatialNavigation/Tags.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import FocusHOC from "./FocusHOC";
 import MenuItems from "./MenuItems";
 import styles from "./spatialComponent.module.css";
@@ -6,10 +7,16 @@ type Props = {};
 const menuItems = ["전체", "VOD", "LIVE 채널", "APP", "지니뮤직", "OTT 서비스"];
 
 function Tags({}: Props) {
+  const [choice, setChoice] = useState<string>("");
   return (
     <nav className={styles.Vertical_Landing}>
       {menuItems.map((item, index) => (
-        <MenuItems key={index} item={item} />
+        <MenuItems
+          key={index}
+          item={item}
+          choice={choice}
+          setChoice={setChoice}
+        />
       ))}
     </nav>
   );
